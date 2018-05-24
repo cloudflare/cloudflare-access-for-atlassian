0103403df2e5e9a86e0e1465b96ce44be896e12d
felipebn <thrash.nation@gmail.com>
2018-05-24 20:32:50 +0100

    Confluence Plugin
    - Replacing the user returned from Crowd with a  Confluence user as it
    prevented admin access

--- confluence-plugin/src/main/java/com/cloudflare/access/atlassian/confluence/auth/ConfluenceSuccessfulAuthenticationRequestHandler.java
+++ confluence-plugin/src/main/java/com/cloudflare/access/atlassian/confluence/auth/ConfluenceSuccessfulAuthenticationRequestHandler.java
@@ -7,10 +7,15 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.atlassian.confluence.security.login.LoginManager;
+import com.atlassian.confluence.security.seraph.ConfluenceUserPrincipal;
+import com.atlassian.confluence.user.ConfluenceUser;
+import com.atlassian.confluence.user.UserAccessor;
 import com.atlassian.crowd.embedded.api.User;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.component.ComponentLocator;
 import com.atlassian.seraph.auth.DefaultAuthenticator;
 import com.atlassian.seraph.service.rememberme.RememberMeService;
@@ -21,11 +26,19 @@ public class ConfluenceSuccessfulAuthenticationRequestHandler implements Success
 
 	private static final Logger log = LoggerFactory.getLogger(ConfluenceSuccessfulAuthenticationRequestHandler.class);
 
+	private UserAccessor userAcessor;
+
+	@Autowired
+	public ConfluenceSuccessfulAuthenticationRequestHandler(@ComponentImport UserAccessor userAcessor) {
+		this.userAcessor = userAcessor;
+	}
+
 	@Override
 	public void handle(HttpServletRequest httpRequest, HttpServletResponse httpResponse, FilterChain chain, User user) {
 		try {
+			ConfluenceUser confluenceUser = userAcessor.getUserByName(user.getName());
 			final HttpSession httpSession = httpRequest.getSession();
-	        httpSession.setAttribute(DefaultAuthenticator.LOGGED_IN_KEY, user);
+	        httpSession.setAttribute(DefaultAuthenticator.LOGGED_IN_KEY, new ConfluenceUserPrincipal(confluenceUser));
 	        httpSession.setAttribute(DefaultAuthenticator.LOGGED_OUT_KEY, null);
 	        ComponentLocator.getComponent(LoginManager.class).onSuccessfulLoginAttempt(user.getName(), httpRequest);
 	        ComponentLocator.getComponent(RememberMeService.class).addRememberMeCookie(httpRequest, httpResponse, user.getName());
